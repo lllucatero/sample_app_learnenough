01d3594de2121b43f39cae3c8405d42d2824370e
lucas lucatero <lcspontes@gmail.com>
2022-01-11 23:58:12 -0300

    Create Users controller, update routes and tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,9 +2,6 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
     
   test "should get root" do
     get root_path
@@ -14,25 +11,25 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
-  end
+    assert_select 'title', full_title
+   end
 
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select 'title', full_title("Help")
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "#{@base_title}"
+    assert_select 'title', full_title("About")
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "#{@base_title}"  
+    assert_select 'title', full_title("Contact")  
   end
     
 end
